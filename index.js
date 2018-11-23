bcf184d24d384dbf1816b55bd0ab1e1421d668d8
Matheus Apolonio <25385108+MatheusApolonio@users.noreply.github.com>
2018-11-23 18:49:50 -0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const request = require('request-promise');
 
 const wishlist = [
-    'WaterCooler',
-    'Mecanico',
-    'Mecânico'
+    'Mecanico   ',
+    'Mecânico   '
 ]
 
 const spotlights = [
-    'Corsair',
-    'Logitech'
+    'G610',
+    'G810',
+    'G910',
+    'G513',
+    'RGB'
 ]
+
+var lastWishlistCount = 0;
+var lastSpotlightCount = 0;
 
 async function getOffers() {
     process.stdout.write('\033c');
@@ -25,23 +30,52 @@ async function getOffers() {
     const products = response.produtos;
     const keys = Object.keys(response.produtos);
 
+    var wishlistCount = 0;
+    var spotlightCount = 0;
+
     for (let i in keys) {
         const product = products[keys[i]];
 
         for (let j in wishlist) {
-            if (product.produto.toLowerCase().includes(wishlist[j].toLowerCase()) && product.quantidade != 0) {
+            if (product.produto.toLowerCase().includes(wishlist[j].trim().toLowerCase()) && product.quantidade != 0) {
+                var isSpotlight = false;
+                wishlistCount++;
 
                 for(let k in spotlights) {
                     if (product.produto.toLowerCase().includes(spotlights[k].toLowerCase())) {
-                        console.log(`\x1b[45m\x1b[4m${wishlist[j]}\x1b[45m\x1b[4m - ${product.produto} - \x1b[1m\x1b[45m\x1b[4mR$ ${product.vlr_oferta}\x1b[45m\x1b[0m`);
+                        isSpotlight = true;
                         break;
                     }
+                }
+
+                if(isSpotlight) {
+                    spotlightCount++;
+                    console.log(`\x1b[45m\x1b[4m${wishlist[j]}\x1b[45m\x1b[4m - ${product.produto} - \x1b[1m\x1b[45m\x1b[4mR$ ${product.vlr_oferta}\x1b[45m\x1b[0m`);
+                    console.log(`\x1b[43mhttps://www.kabum.com.br/produto/${product.codigo}\x1b[0m`);
+                } else {
                     
                     console.log(`\x1b[0m${wishlist[j]}\x1b[0m - ${product.produto} - \x1b[42mR$ ${product.vlr_oferta}\x1b[0m`);
                 }
             }
         }
     }
+
+    console.log();
+
+    if(wishlistCount != lastWishlistCount) {
+        console.log(`\x1b[45mWishlist : ${wishlistCount}\x1b[0m`);
+    } else {
+        console.log(`Wishlist : ${wishlistCount}`);
+    }
+    
+    if(spotlightCount != lastSpotlightCount) {
+        console.log(`\x1b[45mSpotlight: ${spotlightCount}\x1b[0m`);
+    } else {
+        console.log(`Spotlight: ${spotlightCount}`);
+    }
+
+    lastSpotlightCount = spotlightCount;
+    lastWishlistCount = wishlistCount;
 }
 
 getOffers();
