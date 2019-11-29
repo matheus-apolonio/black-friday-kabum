f7e2e969751abecb2d3840850503090522e9d23c
Matheus Apolonio <matheus.apolonio@hotmail.com>
2019-11-28 22:36:38 -0200

    Updated to work with 2019 Black Friday

--- index.js
+++ index.js
@@ -1,83 +1,87 @@
-const request = require('request-promise');
+const request = require("request-promise")
 
 const wishlist = [
-    { regex: /(?=.*water)(?=.*cooler)/g , productName: 'WaterCooler', spotlights: ['Corsair'] },
-    { regex: /(?=.*notebook)/g, productName: 'Notebook', spotlights: ['i3', 'i5'] },
-    { regex: /(?=.*ps4)/g, productName: 'PS4', spotlights: ['Slim'] },
-    { regex: /(?=.*playstation)/g, productName: 'PS4', spotlights: ['Slim'] }
+	{ regex: /(?=.*water)(?=.*cooler)/g, productName: "WaterCooler", spotlights: ["Corsair"] },
+	{ regex: /(?=.*notebook)/g, productName: "Notebook", spotlights: ["i3", "i5"] },
+	{ regex: /(?=.*ps4)/g, productName: "PS4", spotlights: ["Slim"] },
+	{ regex: /(?=.*playstation)/g, productName: "PS4", spotlights: ["Slim"] }
 ]
 
-var lastWishlistCount = 0;
-var lastSpotlightCount = 0;
+var lastWishlistCount = 0
+var lastSpotlightCount = 0
 
 async function getOffers() {
-    process.stdout.write('\033c');
-    const response = await request({
-        url: 'https://cybermonday.kabum.com.br/data.json?campanha=cybermonday',
-        method: 'GET',
-        json: true,
-        headers: {
-            referrer: 'https://cybermonday.kabum.com.br/'
-        }
-    });
+	process.stdout.write("\033c")
+	const response = await request({
+		url:
+			"https://b2lq2jmc06.execute-api.us-east-1.amazonaws.com/PROD/ofertas?campanha=blackfriday&app=1&limite=60&pagina=1",
+		method: "GET",
+		json: true,
+		headers: {
+			referrer: "https://blackfriday.kabum.com.br/"
+		}
+	})
 
-    const products = response.produtos;
-    const keys = Object.keys(response.produtos);
+	const products = response.produtos
+	const keys = Object.keys(response.produtos)
 
-    var wishlistCount = 0;
-    var spotlightCount = 0;
+	var wishlistCount = 0
+	var spotlightCount = 0
 
-    for (let i in keys) {
-        const product = products[keys[i]];
+	for (let i in keys) {
+		const product = products[keys[i]]
 
-        for (let j in wishlist) {
-            let regex = wishlist[j].regex;
+		for (let j in wishlist) {
+			let regex = wishlist[j].regex
 
-            if (regex.test(product.produto.toLowerCase()) && product.quantidade != 0) {
-                wishlistCount++;
+			if (regex.test(product.produto.toLowerCase()) && product.quantidade != 0) {
+				wishlistCount++
 
-                let spotlights = wishlist[j].spotlights;
-                let productName = wishlist[j].productName;
+				let spotlights = wishlist[j].spotlights
+				let productName = wishlist[j].productName
 
-                var isSpotlight = false;
+				var isSpotlight = false
 
-                for(let k in spotlights) {
-                    if (product.produto.toLowerCase().includes(spotlights[k].toLowerCase())) {
-                        isSpotlight = true;
-                        break;
-                    }
-                }
+				for (let k in spotlights) {
+					if (product.produto.toLowerCase().includes(spotlights[k].toLowerCase())) {
+						isSpotlight = true
+						break
+					}
+				}
 
-                if(isSpotlight) {
-                    spotlightCount++;
+				if (isSpotlight) {
+					spotlightCount++
 
-                    console.log(`\x1b[45m\x1b[4m${productName}\x1b[45m\x1b[4m - \x1b[45m${product.produto} - \x1b[1m\x1b[45m\x1b[4mR$ ${product.vlr_oferta}\x1b[45m\x1b[0m`);
-                    console.log(`\x1b[43mhttps://www.kabum.com.br/produto/${product.codigo}\x1b[0m`);
-                } else {
-                    
-                    console.log(`\x1b[0m${productName}\x1b[0m - ${product.produto} - \x1b[42mR$ ${product.vlr_oferta}\x1b[0m`);
-                }
-            }
-        }
-    }
+					console.log(
+						`\x1b[45m\x1b[4m${productName}\x1b[45m\x1b[4m - \x1b[45m${product.produto} - \x1b[1m\x1b[45m\x1b[4mR$ ${product.vlr_oferta}\x1b[45m\x1b[0m`
+					)
+					console.log(`\x1b[43mhttps://www.kabum.com.br/produto/${product.codigo}\x1b[0m`)
+				} else {
+					console.log(
+						`\x1b[0m${productName}\x1b[0m - ${product.produto} - \x1b[42mR$ ${product.vlr_oferta}\x1b[0m`
+					)
+				}
+			}
+		}
+	}
 
-    console.log();
+	console.log()
 
-    if(wishlistCount != lastWishlistCount) {
-        console.log(`\x1b[45mWishlist : ${wishlistCount}\x1b[0m`);
-    } else {
-        console.log(`Wishlist : ${wishlistCount}`);
-    }
-    
-    if(spotlightCount != lastSpotlightCount) {
-        console.log(`\x1b[45mSpotlight: ${spotlightCount}\x1b[0m`);
-    } else {
-        console.log(`Spotlight: ${spotlightCount}`);
-    }
+	if (wishlistCount != lastWishlistCount) {
+		console.log(`\x1b[45mWishlist : ${wishlistCount}\x1b[0m`)
+	} else {
+		console.log(`Wishlist : ${wishlistCount}`)
+	}
 
-    lastSpotlightCount = spotlightCount;
-    lastWishlistCount = wishlistCount;
+	if (spotlightCount != lastSpotlightCount) {
+		console.log(`\x1b[45mSpotlight: ${spotlightCount}\x1b[0m`)
+	} else {
+		console.log(`Spotlight: ${spotlightCount}`)
+	}
+
+	lastSpotlightCount = spotlightCount
+	lastWishlistCount = wishlistCount
 }
 
-getOffers();
-setInterval(getOffers, 60000);
+getOffers()
+setInterval(getOffers, 60000)
